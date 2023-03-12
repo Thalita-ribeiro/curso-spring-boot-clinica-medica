5b47d2a189d8c8a7719817f75bbdc6c2a8b6b3ca
Thalita <sribeiro.thalita@gmail.com>
2023-03-11 22:39:04 -0300

    We use the validations in the dao class

--- src/main/java/med/voll/api/medical/dto/MedicalRegistrationData.java
+++ src/main/java/med/voll/api/medical/dto/MedicalRegistrationData.java
@@ -1,6 +1,24 @@
 package med.voll.api.medical.dto;
 
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import med.voll.api.entity.AddressData;
 
-public record MedicalRegistrationData(String name, String email, String crm, Specialty specialty, AddressData addressData) {
+public record MedicalRegistrationData(
+        @NotBlank
+        String name,
+        @NotBlank
+        @Email
+        String email,
+        @NotBlank
+        @Pattern(regexp = "\\d{4,6}")
+        String crm,
+        @NotNull
+        Specialty specialty,
+        @NotNull
+        @Valid
+        AddressData addressData) {
 }
