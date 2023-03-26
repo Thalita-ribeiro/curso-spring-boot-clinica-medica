28efa1eb5f2ef22f1bbd7fa4ad20b15c23b53f14
Thalita <sribeiro.thalita@gmail.com>
2023-03-25 21:45:48 -0300

    Add method to find active Medicals with pagination

--- src/main/java/med/voll/api/repository/MedicalRepository.java
+++ src/main/java/med/voll/api/repository/MedicalRepository.java
@@ -1,7 +1,10 @@
 package med.voll.api.repository;
 
 import med.voll.api.entity.Medical;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MedicalRepository extends JpaRepository<Medical, Long> {
+    Page<Medical> findAllByIsActiveTrue(Pageable pagination);
 }
